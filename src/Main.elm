adcb260f1fc30bbc5a58137eebcfef9db3928518
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-12-29 14:19:33 +0100

    Playing field

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,35 @@
 module Main exposing (Flags, Model, Msg, main)
 
 import Browser
-import Html exposing (Html, br, div)
+import Html exposing (Html, br, div, node, text)
 import Html.Attributes exposing (style)
 import List.Extra
-import Svg
+import Svg exposing (Svg)
 import Svg.Attributes
 import Theme exposing (Pentomino)
+
+
+playingFieldWidth : Int
+playingFieldWidth =
+    10
+
+
+playingFieldHeight : Int
+playingFieldHeight =
+    20
+
+
+rightPaneWidth : Int
+rightPaneWidth =
+    8
 
 
 type alias Flags =
     ()
 
 
-type alias Model =
-    {}
+type Model
+    = Welcome
 
 
 type alias Msg =
@@ -33,7 +48,7 @@ main =
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( {}
+    ( Welcome
     , Cmd.none
     )
 
@@ -46,7 +61,84 @@ update msg model =
 
 
 view : Model -> Html Msg
-view _ =
+view model =
+    div
+        [ style "width" "100vw"
+        , style "height" "100vh"
+        , style "display" "flex"
+        , style "align-items" "center"
+        , style "justify-content" "center"
+        ]
+        [ node "style" [] [ text """
+            body {
+                margin: 0;
+                background: #333;
+                overflow: hidden;
+            }
+        """ ]
+        , let
+            width : Int
+            width =
+                1 + playingFieldWidth + 1 + rightPaneWidth + 1
+
+            height : Int
+            height =
+                1 + playingFieldHeight + 1
+
+            horizontalLine : Int -> Svg msg
+            horizontalLine y =
+                Svg.line
+                    [ Svg.Attributes.stroke "white"
+                    , Svg.Attributes.x1 "0"
+                    , Svg.Attributes.x2 <| String.fromInt width
+                    , Svg.Attributes.y1 <| String.fromInt y
+                    , Svg.Attributes.y2 <| String.fromInt y
+                    ]
+                    []
+
+            verticalLine : Int -> Svg msg
+            verticalLine x =
+                Svg.line
+                    [ Svg.Attributes.stroke "white"
+                    , Svg.Attributes.x1 <| String.fromInt x
+                    , Svg.Attributes.x2 <| String.fromInt x
+                    , Svg.Attributes.y1 "0"
+                    , Svg.Attributes.y2 <| String.fromInt height
+                    ]
+                    []
+          in
+          (List.map horizontalLine (List.range 0 height)
+            ++ List.map verticalLine (List.range 0 width)
+          )
+            |> Svg.svg
+                [ [ 0
+                  , 0
+                  , width
+                  , height
+                  ]
+                    |> List.map String.fromInt
+                    |> String.join " "
+                    |> Svg.Attributes.viewBox
+                , style "height" "100%"
+                , style "width" "100%"
+                , style "max-height" "calc(100vh - 16px)"
+                , style "max-width" "calc(100vw - 16px)"
+                , Svg.Attributes.stroke "black"
+                , Svg.Attributes.strokeWidth "0.05"
+                ]
+        , case model of
+            Welcome ->
+                viewWelcome
+        ]
+
+
+viewWelcome : Html Msg
+viewWelcome =
+    text ""
+
+
+viewAllPentominos : Html Msg
+viewAllPentominos =
     List.map
         (\( color, pentomino ) ->
             div
@@ -115,9 +207,7 @@ viewPentomino color pentomino =
         viewCell y x cell =
             if cell then
                 Svg.rect
-                    [ Svg.Attributes.stroke "black"
-                    , Svg.Attributes.strokeWidth "0.05"
-                    , Svg.Attributes.fill color
+                    [ Svg.Attributes.fill color
                     , Svg.Attributes.width "1"
                     , Svg.Attributes.height "1"
                     , Svg.Attributes.x (String.fromInt x)
